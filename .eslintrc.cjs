90205f4703f9e64a33b6f5881689a7ac90ccf51c
daiwanxing <377099119@qq.com>
2023-01-29 15:07:58 +0800

    chore(types): add type rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,13 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     root: true,
     env: {
         browser: true,
-        commonjs: true
+        commonjs: true,
     },
-}
+    rules: {
+        "@typescript-eslint/no-explicit-any": [2, { ignoreRestArgs: true }],
+    },
+};
--- rollup.config.ts
+++ rollup.config.ts
@@ -6,35 +6,35 @@ import typescript from "@rollup/plugin-typescript";
 import eslint from "@rollup/plugin-eslint";
 
 export default defineConfig({
-  input: "./src/index.ts",
-  output: [
-    {
-      file: "dist/umd.js",
-      format: "umd",
-      name: "BMapLoader",
-      compact: true,
-    },
-    {
-      file: "dist/index.js",
-      format: "es",
-      compact: true,
-    },
-],
-  plugins: [
-    eslint({
-      include: ["src/**/*.ts"]
-    }),
-    typescript({
-        tsconfig: "./tsconfig.json",
-        throwOnError: true,
-        throwOnWarning: true,
-    }),
-    resolve(),
-    commonjs(),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**",
-      extensions: [".js", ".ts"],
-    }),
-  ],
+    input: "./src/index.ts",
+    output: [
+        {
+            file: "dist/umd.js",
+            format: "umd",
+            name: "BMapLoader",
+            compact: true,
+        },
+        {
+            file: "dist/index.js",
+            format: "es",
+            compact: true,
+        },
+    ],
+    plugins: [
+        eslint({
+            include: ["src/**/*.ts"],
+            throwOnError: true,
+            throwOnWarning: true,
+        }),
+        typescript({
+            tsconfig: "./tsconfig.json",
+        }),
+        resolve(),
+        commonjs(),
+        babel({
+            babelHelpers: "bundled",
+            exclude: "node_modules/**",
+            extensions: [".js", ".ts"],
+        }),
+    ],
 });
